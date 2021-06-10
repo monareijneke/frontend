37ccbe38b9ca9ea904e11d3999ce8390867047dd
monareijneke <83637132+monareijneke@users.noreply.github.com>
2021-06-10 18:14:30 +0200

    Add files via upload

---
+++ JAVA/index.js
@@ -0,0 +1,33 @@
+let naam = 'mona';
+console.log(naam);
+
+//let if =   dit kan niet, omdat if een keyword is
+
+let nummer1 = 4;
+let nummer2 = 4;
+let nummer3 = '4';
+let nummer4 = '4';
+let nummer5 = 10;
+let nummer6 = 1;
+
+console.log(nummer1 + nummer2);
+console.log(nummer3 + nummer4);
+// 2 strings optellen kan niet, hij plakt ze als tekst achter elkaar
+
+console.log(nummer5 / nummer1);
+console.log(nummer5 * nummer6);
+console.log(4 + 4);
+console.log(2 % 10);
+//deze percentage moet ik nog even uitvinden, verder in verdiepen
+console.log(nummer2 - nummer5)
+console.log(nummer2 - nummer5 + nummer6);
+
+let age = 48;
+console.log(age);
+
+let age1 = 'achtenveertig';
+typeof age1
+
+
+
+
