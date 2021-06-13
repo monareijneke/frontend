67f7bfb72c317c089d31c67e478c442cd5a2f144
monareijneke <83637132+monareijneke@users.noreply.github.com>
2021-06-13 13:11:45 +0200

    Add files via upload

---
+++ JAVA_opdracht1/script.js
@@ -0,0 +1,9 @@
+console.log("Hallo Winc Academy studenten")
+//dit is een grote som
+/* let multiple = 1230941 * 1823794
+console.log(multiple)
+let divide = 637263 / 54
+console.log(divide)
+ */
+
+
