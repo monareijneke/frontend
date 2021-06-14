a0101952a15d4b02585b191ac9758293dcf1089d
monareijneke <83637132+monareijneke@users.noreply.github.com>
2021-06-14 08:48:13 +0200

    Add files via upload

---
+++ JAVA_opdracht2/script.js
@@ -0,0 +1,21 @@
+const Age = 17;
+const isFemale = false;
+const driverStatus = 'Bob';
+
+if (Age > 17) {
+    console.log("You can go in.");
+} else {
+    console.log("Sorry, you are too young!");
+}
+
+if (isFemale) {
+    console.log("This person is a Female.");
+} else {
+    console.log("This person is a Man");
+}
+
+if (driverStatus == 'Not Bob') {
+    console.log("This person is the Bob.");
+} else {
+    console.log("This person drunk and cannot drive!")
+}
