a17e0c2adb6095067840778fcac2f4d8faf5bfd7
monareijneke <83637132+monareijneke@users.noreply.github.com>
2021-06-16 09:15:43 +0200

    Add files via upload

---
+++ JAVA_5functions/script.js
@@ -0,0 +1,30 @@
+// define function Pizza
+
+function makingPizza() {
+    console.log("pre-heat your oven");
+    console.log("remove the packaging from your pizza");
+    console.log("put the pizza in your oven");
+    console.log("wait for 12 minutes");
+    console.log("take the pizza out of the oven");
+};
+
+makingPizza();
+makingPizza();
+makingPizza();
+
+//define function Sushi
+
+function orderSushi() {
+    console.log("call the town's Sushi bar");
+    console.log("order a sushi dish");
+    console.log("poor some sojasauce in a small cup")
+};
+
+orderSushi();
+
+//using function multiple times
+orderSushi();
+orderSushi();
+orderSushi();
+makingPizza();
+makingPizza();
