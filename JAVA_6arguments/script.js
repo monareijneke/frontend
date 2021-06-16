2491e962c6f8dae93f2212ac792c26ae01620d17
monareijneke <83637132+monareijneke@users.noreply.github.com>
2021-06-16 22:17:48 +0200

    Add files via upload

---
+++ JAVA_6arguments/script.js
@@ -0,0 +1,8 @@
+const piantingWalls = function (side, color) {
+    console.log("The" + " " + side + "wall has been painted" + " " + color);
+};
+
+const livingroomColor = "grey";
+const WallSides = "north";
+
+piantingWalls(WallSides, livingroomColor);
